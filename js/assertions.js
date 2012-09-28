859e3a966df381c6cbde696c50c34d535f17d30a
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2012-09-28 01:29:49 +0100

    added assertNot()

--- js/assertions.js
+++ js/assertions.js
@@ -2,30 +2,35 @@
 Assertions for a test framework
 */
 
-function Assert(actual){this.actual=actual;}
+function Assert(actual, assertionGender){
+    this.actual=actual;
+    this.notted=!assertionGender;
+    this.Expected=  assertionGender ? "Expected" : "Did not expect";
+}
 
-function assert(actual){return new Assert(actual);}
+function assert(actual){return new Assert(actual, true);}
+function assertNot(actual){return new Assert(actual, false);}
 
 Assert.prototype.IsTrue = function(msg){
-    if(!this.actual){ throw msg||"Expected true but wasn't.";}
+    if( this.actual == this.notted ){ throw msg||this.Expected + " true.";}
 };
 
 Assert.prototype.Equals = function(expected,msg){
-    if(! (this.actual==expected) ){
-        throw msg|| "Expected " + this.actual + " to equal " + expected ;
+    if( (this.actual==expected) == this.notted ){
+        throw msg|| this.Expected + " " + this.actual + " to equal " + expected ;
     }
 };
 
 Assert.prototype.EqualsByPropertyValue = function(expected,msg){
     var actualValue =  JSON.stringify( this.actual );
     var expectedValue = JSON.stringify( expected );
-    if( actualValue != expectedValue ){
-        throw msg|| "Expected " + actualValue + " to equal " + expectedValue ;
+    if( (actualValue == expectedValue) == this.notted ){
+        throw msg|| this.Expected + " " + actualValue + " to equal " + expectedValue + " by value";
     }
 };
 
 $(function(){
-    new Specification("SpecificationsForBasicAsserts",{
+    new Specification("SpecificationsForPrimitiveAssertionPasses",{
         isTrueShouldBeTrueForTrue : function(){
             assert(true).IsTrue();
         },
@@ -35,10 +40,15 @@ $(function(){
         equalsByPropertyValueShouldPassForValueEqualArguments : function(){
             assert( { a: "a", b : function(){ return "b";}} )
                 .EqualsByPropertyValue( { a: "a", b : function(){ return "b";}} );
+        },
+        assertNotShouldReverseTheOutcomeOfAnAssertion : function(){
+            assertNot(false).IsTrue();
+            assertNot(1+1).Equals(3);
+            assertNot({ a: "a"}).EqualsByPropertyValue( { a: "b"});
         }
     }).runTests();
 
-    new Specification("FailureSpecificationsForBasicAsserts",{
+    new Specification("SpecificationsForPrimitiveAssertion_Failures",{
         isTrueShouldNotBeTrueForFalse : function(){
             assert(false).IsTrue();
         },
@@ -50,6 +60,26 @@ $(function(){
         },
         equalsByPropertyValueShouldFailForDifferingProperties : function(){
             assert( { a: "a" } ).EqualsByPropertyValue( { aa: "a" } );
+        },
+        assertNotShouldReverseTheOutcomeOfIsTrue : function(){
+            assertNot(true).IsTrue();
+            assertNot({ a: "a"}).EqualsByPropertyValue( { a: "a"});
+        },
+        assertNotShouldReverseTheOutcomeOfEquals : function(){
+            assertNot(1+1).Equals(2);
+        },
+        assertNotShouldReverseTheOutcomeOfEqualsByValue : function(){
+            assertNot({ a: "a"}).EqualsByPropertyValue( { a: "a"});
+        },
+        assertTrueFailureWithCustomerMessageShouldOutputCustomMessage : function(){
+            assert(false).IsTrue("This assertion failure should output this custom message");
+        },
+        assertEqualsFailureWithCustomerMessageShouldOutputCustomMessage : function(){
+            assert(1).Equals(2, "This assertion failure should output this custom message");
+        },
+        assertEqualsByValueFailureWithCustomerMessageShouldOutputCustomMessage : function(){
+            assert({a:"a"}).EqualsByPropertyValue( 1, "This assertion failure should output this custom message");
         }
+
     }).runTests();
 });
